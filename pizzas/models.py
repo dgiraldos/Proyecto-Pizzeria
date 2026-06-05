bfa4189fd38f49d116355417dec2c6a084329d41
dgiraldos <danielgiral2@gmail.com>
2026-06-05 11:22:58 +0200

    Primer Commit

---
+++ pizzas/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=150)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Pizzas'
+
+    def __str__(self):
+        return self.name
+
+class Ingrediente(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Ingredientes'
+
+    def __str__(self):
+        return self.name
