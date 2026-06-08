25871ca06dc4f6c5b4ad4b53db344273b75f45eb
dgiraldos <danielgiral2@gmail.com>
2026-06-08 11:03:26 +0200

    cambios

---
+++ pizzas/urls.py
@@ -0,0 +1,10 @@
+"""Define patrones de URl para pizzas"""
+
+from django.urls import path
+from . import views
+
+app_name = 'pizzas'
+urlpatterns = [
+    # Página de inicio
+    path('', views.index, name= 'index'),
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """La pagina de inicio de Pizzas"""
+    return render(request, 'pizzas/index.html')
